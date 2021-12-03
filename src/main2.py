259a39f39321874fe091baeeec6afb65fd42a97a
Estela Ramirez <ramirezestela01@gmail.com>
2021-12-03 05:46:55 -0800

    redid UI without MDCard

--- src/main2.py
+++ src/main2.py
@@ -1,11 +1,25 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.animation import Animation
+Window.size = (350, 580)
 
-class MainApp(MDApp):
+class LoginPage(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
-        return Builder.load_file("login.kv")
+        self.theme_cls.primary_palette = "Green"
+        global screen_manager
+        screen_manager = ScreenManager()
+        #screen_manager.add_widget(Builder.load_file("pre-splash.kv"))
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        return screen_manager
+    
+    def on_start(self):
+        Clock.schedule_once(self.login, 5)
 
+    def login(self, *args):
+        screen_manager.current = "login"
 
-MainApp().run()
+LoginPage().run()
