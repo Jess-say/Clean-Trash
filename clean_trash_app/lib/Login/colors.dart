a764943d40fbbe971773a58d6a57f28c2f0e6110
Jess-say <jlu081@ucr.edu>
2022-01-29 15:49:02 -0800

    Login ui

---
+++ clean_trash_app/lib/Login/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColor{
+  static Color boxBackground=const Color(0x99FBFCFF);
+  static Color boxShadowBackground=const Color(0x22FFFFFF);
+  static Color boxText=const Color(0xFF000000);
+  static Color homepageText=const Color(0xCC00FF7F);
+  static Color homePageBackground=const Color(0xFFFBFCFF);
+  static Color nameText=const Color(0x66888888);
+}
---
+++ clean_trash_app/lib/Login/login_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:login/Login/colors.dart' as color;
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top:70, left: 30, right: 30),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  "Clean Trash",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: <Widget>[
+                  inputFile(label: "Username"),
+                  inputFile(label: "Password", obscureText: true)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            // LOGIN BUTTON
+            Column(
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () { /*Should lead to home page*/ },
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // CREATE ACCOUNT BUTTON
+            Column(
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () { /*Should lead to home page*/ },
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      )
+    );
+  }
+
+}
+
+Widget inputFile({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          color: Colors.green,
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.green
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.green
+            )
+          )
+        ),
+      ),
+    ],
+  );
+}
