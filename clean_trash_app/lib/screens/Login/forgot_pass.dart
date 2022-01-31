c4edbd00dab836f8569d3e7e825af9d1ef584aa1
Estela Ramirez <ramirezestela01@gmail.com>
2022-01-30 23:44:53 -0800

    cleaned up forgot password UI

--- clean_trash_app/lib/screens/Login/forgot_pass.dart
+++ clean_trash_app/lib/screens/Login/forgot_pass.dart
@@ -1,133 +1,125 @@
 import 'package:flutter/material.dart';
-import 'colors.dart' as color;
+//import 'colors.dart' as color;
+import '../../theme.dart';
+import 'input_form.dart';
 
 class ForgotPassPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: color.AppColor.homePageBackground,
       body: Container(
-        padding: const EdgeInsets.only(top:100, left: 30, right: 30),
+        padding: const EdgeInsets.only(top: 200, left: 30, right: 30),
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text(
-                  "Reset Password",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: [
+                  Text(
+                    "Reset Password",
+                    style: titleText,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: <Widget>[
-                  inputFile(label: "Email"),
-                  inputFile(label: "New Password", obscureText: true)
                 ],
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Column(
-              children: <Widget>[
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () { /*Should lead to home page*/ },
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.green,
-                      width: 2,
+              const SizedBox(
+                height: 20,
+              ),
+              const InputForm(),
+              const SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {/*Should lead to home page*/},
+                      style: ElevatedButton.styleFrom(
+                        primary: kPrimaryColor, // background
+                        onPrimary: kWhiteColor, // foreground
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 83, vertical: 14),
+                      ),
+                      child: const Text(
+                        "Reset Password",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1,
+                        ), //
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Text(
-                    "Reset Password",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 20,
-                      color: Colors.green,
-                      ),
-                  ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          "Already have an account?",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                        MaterialButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                              "Log in",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: kPrimaryColor,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.green,
-                  ),
-                ),
-                MaterialButton(onPressed: () {
-                  Navigator.pop(context);
-                },
-                    child: Text(
-                      "Log in",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    )
-                ),
-              ],
-            )
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget inputFile({label, obscureText = false}) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w300,
-          color: Colors.green,
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-      TextField(
-        obscureText: obscureText,
-        decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color: Colors.green
-              ),
-            ),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: Colors.green
-                )
-            )
-        ),
-      ),
-    ],
-  );
-}
+// Widget inputFile({label, obscureText = false}) {
+//   return Column(
+//     crossAxisAlignment: CrossAxisAlignment.start,
+//     children: <Widget>[
+//       Text(
+//         label,
+//         style: TextStyle(
+//           fontSize: 20,
+//           fontWeight: FontWeight.w300,
+//           color: Colors.green,
+//         ),
+//       ),
+//       SizedBox(
+//         height: 15,
+//       ),
+//       TextField(
+//         obscureText: obscureText,
+//         decoration: InputDecoration(
+//             contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+//             enabledBorder: OutlineInputBorder(
+//               borderSide: BorderSide(
+//                   color: Colors.green
+//               ),
+//             ),
+//             border: OutlineInputBorder(
+//                 borderSide: BorderSide(
+//                     color: Colors.green
+//                 )
+//             )
+//         ),
+//       ),
+//     ],
+//   );
+// }
