47e315d2e27e5dc01b8d8863e42abef147cd4ab0
Estela Ramirez <ramirezestela01@gmail.com>
2022-01-31 12:40:42 -0800

    cleaned up create account UI

--- clean_trash_app/lib/screens/Login/create_account_page.dart
+++ clean_trash_app/lib/screens/Login/create_account_page.dart
@@ -1,103 +1,86 @@
 import 'package:flutter/material.dart';
-
-import 'colors.dart' as color;
+//import 'colors.dart' as color;
+import '../../theme.dart';
+import 'signup_form.dart';
+import 'checkbox.dart';
 
 class CreateAccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: color.AppColor.homePageBackground,
-      body: SingleChildScrollView(
-
-        child: Container(
-          padding: const EdgeInsets.only(top:80, left: 30, right: 30),
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
+      resizeToAvoidBottomInset: true, // avoids text field being covered by keyboard.
+      body: Container(
+        padding: const EdgeInsets.only(top:120, left: 30, right: 30),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text(
-                    "Create Account",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
+              Text(
+                "Create Account",
+                style: titleText,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SignUpForm(),
+              const SizedBox(
+                height: 20,
+              ),
+              const CheckBox(text: "Agree to terms and conditions."),
+              const SizedBox(
+                height: 20,
+              ),
+              const CheckBox(text: "I am at least 13 years old."),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                  child: ElevatedButton(
+                    onPressed: () {/* create the account */},
+                    style: ElevatedButton.styleFrom(
+                        primary: kPrimaryColor, // background
+                        onPrimary: kWhiteColor, // foreground
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 120, vertical: 15),
+                      ),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.2,
+                      ),
+                    )
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              Column(
-                children: <Widget>[
-                  inputFile(label: "Email:"),
-                  inputFile(label: "Username:"),
-                  inputFile(label: "Password", obscureText: true),
-                  inputFile(label: "Confirm Password: ", obscureText: true)
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 3, left: 3),
-                decoration:
-                  BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border(
-                      bottom: BorderSide(color: Colors.green),
-                      top: BorderSide(color: Colors.green),
-                      left: BorderSide(color: Colors.green),
-                      right: BorderSide(color: Colors.green),
-                    ),
-                  ),
-                child: MaterialButton(
-                  minWidth: double.infinity,
-                  height: 60,
-                  onPressed: () {/* create the account */},
-                  color: Colors.white,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Colors.green,
-                    ),
-                  )
                 ),
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
+                  const Text(
                     "Already have an account?",
                     style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 14,
                       fontWeight: FontWeight.w400,
-                      color: Colors.green,
+                      color: kPrimaryColor,
                     ),
                   ),
                   MaterialButton(onPressed: () {
                     Navigator.pop(context);
                   },
-                      child: Text(
+                      child: const Text(
                         "Log in",
                         style: TextStyle(
                           fontSize: 15,
                           fontWeight: FontWeight.bold,
-                          color: Colors.green,
+                          color: kPrimaryColor,
                         ),
                       )
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
@@ -106,37 +89,37 @@ class CreateAccountPage extends StatelessWidget {
   }
 }
 
-Widget inputFile({label, obscureText = false}) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w300,
-          color: Colors.green,
-        ),
-      ),
-      SizedBox(
-        height: 15,
-      ),
-      TextField(
-        obscureText: obscureText,
-        decoration: InputDecoration(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color: Colors.green
-              ),
-            ),
-            border: OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: Colors.green
-                )
-            )
-        ),
-      ),
-    ],
-  );
-}
+// Widget inputFile({label, obscureText = false}) {
+//   return Column(
+//     crossAxisAlignment: CrossAxisAlignment.start,
+//     children: <Widget>[
+//       Text(
+//         label,
+//         style: TextStyle(
+//           fontSize: 20,
+//           fontWeight: FontWeight.w300,
+//           color: Colors.green,
+//         ),
+//       ),
+//       SizedBox(
+//         height: 15,
+//       ),
+//       TextField(
+//         obscureText: obscureText,
+//         decoration: InputDecoration(
+//             contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+//             enabledBorder: OutlineInputBorder(
+//               borderSide: BorderSide(
+//                   color: Colors.green
+//               ),
+//             ),
+//             border: OutlineInputBorder(
+//                 borderSide: BorderSide(
+//                     color: Colors.green
+//                 )
+//             )
+//         ),
+//       ),
+//     ],
+//   );
+// }
