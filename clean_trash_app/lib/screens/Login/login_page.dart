f905dccd074e3b5455acf3e858f879deacf6f40c
Estela Ramirez <ramirezestela01@gmail.com>
2022-01-30 23:44:05 -0800

    made widget placement more like forgot_pass.dart

--- clean_trash_app/lib/screens/Login/login_page.dart
+++ clean_trash_app/lib/screens/Login/login_page.dart
@@ -9,97 +9,99 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: kDefaultPadding,
+      body: Container(
+        padding: const EdgeInsets.only(top: 200, left: 30, right: 30),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
         child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 200,
-              ),
-              Text(
-                "Welcome Back",
-                style: titleText,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const InputForm(),
-              const SizedBox(
-                height: 20,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ForgotPassPage()));
-                },
-                child: const Text(
-                  "Forgot password?",
-                  style: TextStyle(
-                    color: kZambeziColor,
-                    fontSize: 14,
-                    decoration: TextDecoration.underline,
-                    decorationThickness: 1,
-                  ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                Text(
+                  "Welcome Back",
+                  style: titleText,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const InputForm(),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ForgotPassPage()));
+              },
+              child: const Text(
+                "Forgot password?",
+                style: TextStyle(
+                  color: kZambeziColor,
+                  fontSize: 14,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 1,
                 ),
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: kPrimaryColor, // background
-                    onPrimary: kWhiteColor, // foreground
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 125, vertical: 15),
-                  ),
-                  child: const Text(
-                    "LOG IN",
-                    style: TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 2.2,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: Column(
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: kPrimaryColor, // background
+                      onPrimary: kWhiteColor, // foreground
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 125, vertical: 15),
+                    ),
+                    child: const Text(
+                      "LOG IN",
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.2,
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Center(
-                child: OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CreateAccountPage()));
-                  },
-                  style: OutlinedButton.styleFrom(
-                    primary: kPrimaryColor,
-                    side: const BorderSide(width: 0.5, color: kDarkGreyColor),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 120, vertical: 15),
+                  const SizedBox(
+                    height: 40,
                   ),
-                  child: const Text(
-                    "SIGN UP",
-                    style: TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 2.2,
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CreateAccountPage()));
+                    },
+                    style: OutlinedButton.styleFrom(
+                      primary: kPrimaryColor,
+                      side: const BorderSide(width: 0.5, color: kDarkGreyColor),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 120, vertical: 15),
+                    ),
+                    child: const Text(
+                      "SIGN UP",
+                      style: TextStyle(
+                        fontSize: 14,
+                        letterSpacing: 2.2,
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }
