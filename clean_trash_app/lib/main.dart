ecdc3fa9c71a016a29bc7d9976d9ae59cc867f8b
briortega <briortega14@gmail.com>
2022-01-31 23:07:03 -0800

    Added google-services.json file and new main.dart

--- clean_trash_app/lib/main.dart
+++ clean_trash_app/lib/main.dart
@@ -1,33 +1,38 @@
-import 'package:cleantrash_app/screens/home/home_screen.dart';
+import 'package:cleantrash_app/screens/Login/login_page.dart';
+import 'package:cleantrash_app/screens/Login/user/auth.dart';
 import 'package:flutter/material.dart';
-import 'screens/home/home_view.dart';
-import 'dart:async';
-import 'dart:io';
+import 'package:provider/provider.dart';
+//import 'Login/login_page.dart';
+//import 'user/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-//import 'package:camera/camera.dart';
-Future<void> main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-//
-  //// Obtain a list of the available cameras on the device.
-  //final cameras = await availableCameras();
-//
-  //// Get a specific camera from the list of available cameras.
-  //final firstCamera = cameras.first;
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Clean_Trash',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return MultiProvider(
+      providers: [
+        Provider<Auth>(
+          create: (_) => Auth(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          primarySwatch: Colors.green,
+        ),
+        home: LoginPage(),
       ),
-      home: HomeScreen(),
     );
   }
 }
