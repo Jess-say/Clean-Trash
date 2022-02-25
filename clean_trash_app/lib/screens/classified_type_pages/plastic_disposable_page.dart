0376c510d485bc0a3ff77c82ad543063bb8ee707
briortega <briortega14@gmail.com>
2022-02-24 21:12:38 -0800

    added a page for plastic disposable which include plastic food cup/containers and added pages for plastic utensils and plates/cups that maybe be recyclable? according to site

---
+++ clean_trash_app/lib/screens/classified_type_pages/plastic_disposable_page.dart
@@ -0,0 +1,294 @@
+import 'package:flutter/material.dart';
+import '/screens/home/home_view.dart';
+import 'components/custom_alert.dart';
+
+class PlasticDisposablePage extends StatefulWidget {
+  @override
+  _PlasticDisposablePageState createState() => _PlasticDisposablePageState();
+}
+
+class _PlasticDisposablePageState extends State<PlasticDisposablePage> {
+  @override
+  Widget build(BuildContext context) {
+    final fwidth = MediaQuery.of(context).size.width;
+    final fheight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text(""), actions: <Widget>[
+        IconButton(
+            icon: const Icon(
+              Icons.check,
+              color: Colors.white,
+              size: 30,
+            ),
+            padding: const EdgeInsets.only(right: 15),
+            onPressed: () {
+              _appModalBottomSheet(context);
+            })
+      ]),
+      body: Container(
+        child: Image.asset('assets/images/disposable_plastic1.jpg',
+            height: fheight, width: fwidth, fit: BoxFit.fitWidth),
+      ),
+    );
+  }
+}
+
+void _appModalBottomSheet(context) {
+  showModalBottomSheet(
+    isScrollControlled: true,
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0), topRight: Radius.circular(15.0)),
+    ),
+    builder: (context) => SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * .65,
+          child: ListView(
+            children: [
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      //const Spacer(),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          print("X Pressed");
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomeView()),
+                          );
+                        },
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+                    child: Row(children: const [
+                      Expanded(
+                        child: Text(
+                          "Similar Items",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Container(
+                            //color: primaryColor,
+                            margin: const EdgeInsets.all(8),
+                            height: 120,
+                            width: 125,
+                            child: Card(
+                              child: Container(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Image.asset('assets/images/disposable_plastic2.jpg'),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                side: const BorderSide(
+                                    color: Color.fromARGB(255, 214, 214, 214),
+                                    width: 1),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 214, 214, 214),
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 7),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            //color: primaryColor,
+                            margin: const EdgeInsets.all(8),
+                            height: 120,
+                            width: 125,
+                            child: Card(
+                              child: Container(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Image.asset('assets/images/disposable_plastic3.jpg'),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 214, 214, 214),
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 7),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            //color: primaryColor,
+                            margin: const EdgeInsets.all(8),
+                            height: 120,
+                            width: 125,
+                            child: Card(
+                              child: Container(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Image.asset('assets/images/disposable_plastic4.jpg'),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                side: const BorderSide(
+                                    color: Color.fromARGB(255, 206, 204, 204),
+                                    width: 1),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 214, 214, 214),
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 7),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            //color: primaryColor,
+                            margin: const EdgeInsets.all(8),
+                            height: 120,
+                            width: 125,
+                            child: Card(
+                              child: Container(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Image.asset('assets/images/disposable_plastic5.jpg'),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromARGB(255, 214, 214, 214),
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 7),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      )),
+                  infoSection0,
+                  infoSection1,
+                  ElevatedButton(
+                    style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(18.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )),
+                    onPressed: () {
+                      showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (_) => const CustomAlert(
+                            "Awesome! You have prevented recycling contamination."),
+                      );
+                      print("pressed Throw away Button");
+                    },
+                    child: const Text('Throw away'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget infoSection0 = Container(
+  padding: const EdgeInsets.all(12),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: 5, top: 5),
+              child: const Text(
+                'It\'s Not Recyclable!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              "Don't Recyle in Riverside County.",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
+
+Widget infoSection1 = Container(
+  padding: const EdgeInsets.all(12),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: const Text(
+                'Instructions!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              "Throw away in trash bag.",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
