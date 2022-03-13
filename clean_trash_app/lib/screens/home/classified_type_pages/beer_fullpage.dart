4ed42d37989cfc61e27e17ce0d9c54912c968010
ChuangMandy <mandy840907@gmail.com>
2022-03-13 13:12:47 -0700

    remove stream

--- clean_trash_app/lib/screens/home/classified_type_pages/beer_fullpage.dart
+++ clean_trash_app/lib/screens/home/classified_type_pages/beer_fullpage.dart
@@ -11,214 +11,184 @@ class BeerFullPage extends StatefulWidget {
 }
 
 class _BeerFullPageState extends State<BeerFullPage> {
-  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final fwidth = MediaQuery.of(context).size.width;
     final fheight = MediaQuery.of(context).size.height;
-    User user = Provider.of<User>(context);
-    return StreamBuilder<UserData>(
-        stream: DatabaseService(uid: user.uid).userData,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            UserData? userData = snapshot.data;
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text(""),
-              ),
-              body: Container(
-                color: kBackgroundColor,
-                child: ListView(
-                  children: [
-                    Column(
-                      children: <Widget>[
-                        const SizedBox(
-                          height: 30,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+      ),
+      body: Container(
+        color: kBackgroundColor,
+        child: ListView(
+          children: [
+            Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+                  child: Row(children: const [
+                    Expanded(
+                      child: Text(
+                        "Similar Items",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ]),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          //color: primaryColor,
+                          margin: const EdgeInsets.all(8),
+                          height: 120,
+                          width: 125,
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.asset('assets/images/alcohol2.jpg'),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                  color: Color.fromARGB(255, 214, 214, 214),
+                                  width: 1),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 214, 214, 214),
+                                blurRadius: 5.0,
+                                offset: Offset(0, 7),
+                              ),
+                            ],
+                          ),
                         ),
                         Container(
-                          alignment: Alignment.topLeft,
-                          padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
-                          child: Row(children: const [
-                            Expanded(
-                              child: Text(
-                                "Similar Items",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
+                          margin: const EdgeInsets.all(8),
+                          height: 120,
+                          width: 125,
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.asset('assets/images/beer1.jpg'),
                             ),
-                          ]),
+                            shape: RoundedRectangleBorder(
+                              //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 214, 214, 214),
+                                blurRadius: 5.0,
+                                offset: Offset(0, 7),
+                              ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(
-                          height: 10,
+                        Container(
+                          //color: primaryColor,
+                          margin: const EdgeInsets.all(8),
+                          height: 120,
+                          width: 125,
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.asset('assets/images/beer3.jpg'),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                  color: Color.fromARGB(255, 206, 204, 204),
+                                  width: 1),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 214, 214, 214),
+                                blurRadius: 5.0,
+                                offset: Offset(0, 7),
+                              ),
+                            ],
+                          ),
                         ),
-                        SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                Container(
-                                  //color: primaryColor,
-                                  margin: const EdgeInsets.all(8),
-                                  height: 120,
-                                  width: 125,
-                                  child: Card(
-                                    child: Container(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Image.asset(
-                                          'assets/images/alcohol2.jpg'),
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                      side: const BorderSide(
-                                          color: Color.fromARGB(
-                                              255, 214, 214, 214),
-                                          width: 1),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  decoration: const BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            Color.fromARGB(255, 214, 214, 214),
-                                        blurRadius: 5.0,
-                                        offset: Offset(0, 7),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.all(8),
-                                  height: 120,
-                                  width: 125,
-                                  child: Card(
-                                    child: Container(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Image.asset(
-                                          'assets/images/beer1.jpg'),
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                      //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  decoration: const BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            Color.fromARGB(255, 214, 214, 214),
-                                        blurRadius: 5.0,
-                                        offset: Offset(0, 7),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  //color: primaryColor,
-                                  margin: const EdgeInsets.all(8),
-                                  height: 120,
-                                  width: 125,
-                                  child: Card(
-                                    child: Container(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Image.asset(
-                                          'assets/images/beer3.jpg'),
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                      side: const BorderSide(
-                                          color: Color.fromARGB(
-                                              255, 206, 204, 204),
-                                          width: 1),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  decoration: const BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            Color.fromARGB(255, 214, 214, 214),
-                                        blurRadius: 5.0,
-                                        offset: Offset(0, 7),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  //color: primaryColor,
-                                  margin: const EdgeInsets.all(8),
-                                  height: 120,
-                                  width: 125,
-                                  child: Card(
-                                    child: Container(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Image.asset(
-                                          'assets/images/beer2.jpg'),
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                      //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                  ),
-                                  decoration: const BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color:
-                                            Color.fromARGB(255, 214, 214, 214),
-                                        blurRadius: 5.0,
-                                        offset: Offset(0, 7),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            )),
-                        const SizedBox(
-                          height: 20,
+                        Container(
+                          //color: primaryColor,
+                          margin: const EdgeInsets.all(8),
+                          height: 120,
+                          width: 125,
+                          child: Card(
+                            child: Container(
+                              padding: const EdgeInsets.all(4.0),
+                              child: Image.asset('assets/images/beer2.jpg'),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              //side: const BorderSide(color: Color.fromARGB(255, 255, 254, 254), width: 1),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromARGB(255, 214, 214, 214),
+                                blurRadius: 5.0,
+                                offset: Offset(0, 7),
+                              ),
+                            ],
+                          ),
                         ),
-                        infoSection0,
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        infoSection1,
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        ElevatedButton(
-                          style: TextButton.styleFrom(
-                              padding: const EdgeInsets.all(18.0),
-                              primary: Colors.white,
-                              textStyle: const TextStyle(fontSize: 18),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              )),
-                          onPressed: () async {
-                            showDialog(
-                              barrierDismissible: false,
-                              context: context,
-                              builder: (_) => const CustomAlert(
-                                  "Awesome! You logged your first item! Did you know that Americans dispose of 10 million metric tons of glass annually."),
-                            );
-                            if (_formKey.currentState!.validate()) {
-                              await DatabaseService(uid: user.uid)
-                                  .updateUserData(
-                                      userData?.cardboard ?? 0,
-                                      (userData?.glass ?? 0) + 1,
-                                      userData?.metal ?? 0,
-                                      userData?.paper ?? 0,
-                                      userData?.plastic ?? 0);
-                            }
-                            print("pressed Recycle Button");
-                          },
-                          child: const Text('Recycle'),
-                        )
                       ],
-                    ),
-                  ],
+                    )),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            );
-          } else {
-            return Scaffold();
-          }
-        });
+                infoSection0,
+                const SizedBox(
+                  height: 20,
+                ),
+                infoSection1,
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(18.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 18),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                  onPressed: () async {
+                    showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (_) => const CustomAlert(
+                          "Awesome! You logged your first item! Did you know that Americans dispose of 10 million metric tons of glass annually."),
+                    );
+                    print("pressed Recycle Button");
+                  },
+                  child: const Text('Recycle'),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
