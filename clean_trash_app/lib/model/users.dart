024e56d6ca2f863316d8df3d99afa762088d7891
briortega <briortega14@gmail.com>
2022-03-14 02:51:59 -0700

    Not sure if something I changed works with current code so to be safe I'm pushing code because it works

--- clean_trash_app/lib/model/users.dart
+++ clean_trash_app/lib/model/users.dart
@@ -1,10 +1,10 @@
-class User {
-  final String? uid;
-  final int? cardboard;
-  final int? glass;
-  final int? metal;
-  final int? paper;
-  final int? plastic;
+class User{
+  late final String? uid;
+  late final int? cardboard;
+  late final int? glass;
+  late final int? metal;
+  late final int? paper;
+  late final int? plastic;
 
   User(
       {this.uid,
@@ -13,7 +13,34 @@ class User {
       this.metal,
       this.paper,
       this.plastic});
+
+  factory User.fromJson(Map<String, dynamic> json) =>
+      _userFromJson(json);
+  Map<String, dynamic> toJson() => _userToJson(this);
+
+
 }
+
+User _userFromJson(Map<String, dynamic> json) {
+  return User(
+    uid: ['uid'] as String,
+    cardboard: json['cardboard'] as int,
+    glass: json['glass'] as int,
+    metal: json['metal'] as int,
+    paper: json['paper'] as int,
+    plastic: json['plastic'] as int
+  );
+}
+// 2
+Map<String, dynamic> _userToJson(User instance) =>
+    <String, dynamic>{
+      'uid': instance.uid,
+      'cardboard': instance.cardboard,
+      'glass': instance.glass,
+      'metal': instance.metal,
+      'paper': instance.paper,
+      'plastic': instance.plastic,
+    };
 
 class UserData {
   final String? uid;
